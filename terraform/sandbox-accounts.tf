9584bbc724b5cdf95dc73c6883174d5cf7b32b93
Bckarnati <Bckarnati@gmail.com>
2024-01-29 16:35:02 -0500

    Updated Snadbox account to test the AFT modules

---
+++ terraform/sandbox-accounts.tf
@@ -0,0 +1,29 @@
+module "sandbox_account_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aakarnati+sandbox01@gmail.com"
+    AccountName               = "sandbox-account-01"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "aakarnati+sandbox01@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "BOOTCAMP:Owner" = "aakarnati@gmail.com"
+    "BOOTCAMP:Environment" = "Dev"
+    "BOOTCAMP:Project" = "AFT Tutorial"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "BOOTCAMP"
+    change_reason       = "BOOTCAMP AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox-customizations"
+}
