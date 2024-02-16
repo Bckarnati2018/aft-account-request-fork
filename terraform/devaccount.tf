4ebf9313600f8c246f9841a5f9c84b0c799fcbd2
Bckarnati <Bckarnati@gmail.com>
2024-02-16 15:24:44 -0500

    retry the sandbox account after deeting the dupicate entry in dynamodb

--- terraform/devaccount.tf
+++ terraform/devaccount.tf
@@ -1,4 +1,4 @@
-module "sandbox_account_01" {
+module "Devbox_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
@@ -26,34 +26,4 @@ module "sandbox_account_01" {
   }
 
   account_customizations_name = "devbox"
-}
-
-module "sandbox_account_01" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "aakarnati+sandbox01@gmail.com"
-    AccountName               = "sandbox-account-01"
-    ManagedOrganizationalUnit = "LearnAFT(ou-4ymo-sjlq2een)"
-    SSOUserEmail              = "aakarnati+sandbox01@gmail.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "User"
-  }
-
-  account_tags = {
-    "BOOTCAMP:Owner" = "aakarnati@gmail.com"
-    "BOOTCAMP:Environment" = "Dev"
-    "BOOTCAMP:Project" = "AFT Tutorial"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "BOOTCAMP"
-    change_reason       = "BOOTCAMP AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
 }
